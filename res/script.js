30f8eefc0756d53e8a65549b0a9c357fe8b3b176
Kaffite <karlmarkushannust@gmail.com>
2025-12-04 19:18:51 +0200

    Added Sleepsort

--- res/script.js
+++ res/script.js
@@ -11,14 +11,15 @@ function changeBtn(sorted, parent){
 }
 
 function arrSorted(parent, length){
+    let sorted = true;
     for (i = 1; i < length; i++){
         let previousElement = document.getElementById(parent + "Box" + (i))
         let currentElement = document.getElementById(parent + "Box" + (i+1))
         if (currentElement.innerHTML < previousElement.innerHTML){
-            return false;
+            sorted = false;
         }
     }
-    return true;
+    changeBtn(sorted, parent);
 }
 
 function bogoSort(){
@@ -30,12 +31,30 @@ function bogoSort(){
     for (i = 0; i < arrLength; i++){
         let randomIndex = Math.floor(Math.random() * arr.length);
         let value = arr.splice(randomIndex, 1);
-        let currentElement = document.getElementById(parent + "Box" + (i+1));
+        let currentElement = document.getElementById(parent + "Box" + (i + 1));
         currentElement.innerHTML = value;
     }
-
-    let sorted = arrSorted(parent, arrLength);
-    changeBtn(sorted, parent);
+    arrSorted(parent, arrLength);
 }
 
-numbers = [1, 2, 3, 4];
+function sleepsort(){
+    let parent = "sleep";
+    let arr = [5, 7, 3, 1];
+    // reset the values
+    for (i = 0; i < arr.length; i++){
+        let el = document.getElementById(parent + "Box" + (i + 1));
+        el.innerHTML = "x";
+    }
+    // Sort the array
+    let boxNumber = 1;
+    for (i = 0; i < arr.length; i++){
+        let number = arr[i];
+        setTimeout(() =>{
+            let el = document.getElementById(parent + "Box" + boxNumber);
+            el.innerHTML = number;
+            boxNumber++;
+    }, number * 1000);
+    }
+}
+
+numbers = [4, 1, 3, 2];
